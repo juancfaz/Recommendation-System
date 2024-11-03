9ccdba01b124cf6ee77ac18e99ed91a05b7ef9ae
Juan Carlos Faz Leal <juancarlosfazleal@Juans-MacBook-Air.local>
2024-11-03 02:37:10 -0700

    MR

--- MR-Colaborativo/filtrado_colaborativo.py
+++ MR-Colaborativo/filtrado_colaborativo.py
@@ -9,43 +9,33 @@ def cargar_datos():
 
 def recomendar_peliculas_colaborativas(user_id, ratings, movies, top_n=5):
     """Recomendar películas utilizando filtrado colaborativo."""
-    # Crear matriz de utilidad
     X = ratings.pivot(index='userId', columns='movieId', values='rating').fillna(0)
     
-    # Calcular la similitud entre usuarios
     user_similarity = cosine_similarity(X)
     user_similarity_df = pd.DataFrame(user_similarity, index=X.index, columns=X.index)
 
-    # Obtener similitudes para el usuario objetivo
     similar_users = user_similarity_df[user_id].sort_values(ascending=False)
     
-    # Obtener películas que el usuario ha visto
     watched_movies = X.loc[user_id][X.loc[user_id] > 0].index
     
-    # Inicializar un diccionario para las puntuaciones
     movie_scores = {}
 
-    # Sumar las puntuaciones de usuarios similares
     for similar_user, similarity_score in similar_users.items():
-        if similar_user != user_id:  # Ignorar al usuario mismo
-            # Obtener las películas que el usuario similar ha visto y el puntaje
+        if similar_user != user_id:
             for movie_id in X.loc[similar_user][X.loc[similar_user] > 0].index:
                 if movie_id not in watched_movies:
                     if movie_id not in movie_scores:
                         movie_scores[movie_id] = 0
                     movie_scores[movie_id] += similarity_score * X.loc[similar_user, movie_id]
 
-    # Ordenar las películas recomendadas
     recommended_movies = sorted(movie_scores.items(), key=lambda x: x[1], reverse=True)
 
-    # Devolver las mejores N recomendaciones
     recommended_movie_ids = [movie_id for movie_id, score in recommended_movies[:top_n]]
     
     return movies[movies['movieId'].isin(recommended_movie_ids)]['title']
 
-# Cargar datos y realizar recomendaciones
 movies, ratings = cargar_datos()
-user_id = 4  # Cambia el ID del usuario según sea necesario
+user_id = 4
 recommendations = recomendar_peliculas_colaborativas(user_id, ratings, movies, top_n=5)
 print("Recomendaciones para el usuario: ")
 print(recommendations)
--- MR-Contenido/filtrado_contenido.py
+++ MR-Contenido/filtrado_contenido.py
@@ -10,33 +10,24 @@ def cargar_datos():
 
 def recomendar_peliculas_contenido(user_id, ratings, movies, top_n=5):
     """Recomendar películas utilizando filtrado basado en contenido."""
-    # Construir el perfil del usuario (movieId, rating)
     user_ratings = ratings[ratings['userId'] == user_id][['movieId', 'rating']]
     
-    # Codificar los generos de las películas con el metodo one hot encoding (OHE)
     genres = movies['genres'].str.get_dummies(sep='|')
     
-    # Filtrar los géneros solo para las películas que el usuario ha calificado
     rated_movies = movies[movies['movieId'].isin(user_ratings['movieId'])]
     rated_genres = genres.loc[rated_movies.index].values
     
-    # Filtrar las calificaciones de las películas que el usuario ha calificado
     ratings = (user_ratings['rating'].values * 2) - 1
 
-    # Calcular el perfil del usuario
     user_profile = rated_genres.T.dot(ratings)
     
-    # Ponderacion para llevar los valores a una escala de 0 a 1
     user_profile = user_profile / user_profile.sum()
     
-    # Que peliculas no ha visto el usuario
     unseen_movies = movies[~movies['movieId'].isin(user_ratings['movieId'])]
     unrated_genres = genres.loc[unseen_movies.index]
     
-    # Multiplicar matriz de perfil de usuario x géneros de películas no calificadas
     scores = unrated_genres.dot(user_profile)
     
-    # Retornar las top_n películas recomendadas
     return movies.loc[scores.sort_values(ascending=False).head(top_n).index]
 
 movies, ratings = cargar_datos()
